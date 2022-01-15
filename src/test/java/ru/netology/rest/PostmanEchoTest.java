79f4612cb23a39146381bb8127fa322b169624c9
Pavel-Lokhmatov <hlest.ivsvnyby@gmail.com>
2022-01-15 15:04:27 +0400

    check post query test pass

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+public class PostmanEchoTest {
+    @Test
+    void shouldPostEcho() {
+        // Given - When - Then
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+}
